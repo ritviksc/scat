56b6df764b77ad024dd3d09a75daf7f72a925482
ritviksc <contact4ritvik@gmail.com>
2026-01-18 02:39:04 -0800

    Inital SCAT v.0.0.1 in awk

---
+++ tests/header_check.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_CHECK_H
+#define HEADER_CHECK_H
+
+
+/* This is a multiline comment
+ * spanning
+ * multiple lines */
+
+/* Another
+ * multiline
+ */
+
+//TODO
+
+// #endif //HEADER_CHECK_H
